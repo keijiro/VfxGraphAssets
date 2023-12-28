6ca797c28c1fd5c98fdc5d52af361b7b762d7baf
Keijiro Takahashi <keijiro@gmail.com>
2023-12-28 13:51:31 +0900

    Addition: MeshFilter property binder

---
+++ Packages/jp.keijiro.vfxgraphassets/Scripts/VFXMeshFilterBinder.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
+
+namespace Klak.Vfx {
+
+[AddComponentMenu("VFX/Property Binders/Mesh Filter Binder")]
+[VFXBinder("Utility/Mesh Filter")]
+public sealed class VFXMeshFilterBinder : VFXBinderBase
+{
+    public string MeshProperty
+      { get => (string)_meshProperty;
+        set => _meshProperty = value; }
+
+    [VFXPropertyBinding("UnityEngine.Mesh"), SerializeField]
+    ExposedProperty _meshProperty = "Mesh";
+
+    public MeshFilter Target = null;
+
+    public override bool IsValid(VisualEffect component)
+      => Target != null && component.HasMesh(_meshProperty);
+
+    public override void UpdateBinding(VisualEffect component)
+      => component.SetMesh(_meshProperty, Target.sharedMesh);
+
+    public override string ToString()
+      => $"MeshFilter : '{_meshProperty}' -> {Target?.name ?? "(null)"}";
+}
+
+} // namespace Klak.Vfx
